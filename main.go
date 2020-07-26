864456d167703b3f9277889a6ff37831f3c4099a
zanema <amenaz1@gmail.com>
2020-07-25 22:15:39 -0500

    add perf profiling and INSERT option flag

--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"database/sql"
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"github.com/google/uuid"
 	"github.com/lib/pq"
+	"github.com/pkg/profile"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"os"
+	"path/filepath"
 	"runtime"
 )
 
@@ -37,12 +40,24 @@ usage: %s
 Options:
 `
 	var rows int
+	var useInsert, memProfile, cpuProfile bool
 	flag.IntVar(&rows, "r", 1000, "number of rows to add")
+	flag.BoolVar(&useInsert, "insert", false, "use insert instead of COPY")
+	flag.BoolVar(&memProfile, "mem-profile", false, "run memory profiling")
+	flag.BoolVar(&cpuProfile, "cpu-profile", false, "run cpu profiling")
+
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, usage, os.Args[0])
 		flag.PrintDefaults()
 	}
 	flag.Parse()
+
+	if cpuProfile {
+		defer profile.Start(profile.CPUProfile, profile.ProfilePath(".")).Stop()
+	}
+	if memProfile {
+		defer profile.Start(profile.MemProfile, profile.ProfilePath("."), profile.NoShutdownHook).Stop()
+	}
 
 	cmdString := "user=ctdownloader dbname=ctdownload sslmode=disable"
 
@@ -56,18 +71,22 @@ Options:
 		log.Fatal(err)
 	}
 
+	if useInsert {
+		for i := 0; i < rows; i++ {
+			if i%10000 == 0 {
+				log.Infof("Writing %d rows to temp file...", i)
+			}
 
-	for i := 0; i < rows; i++ {
-		if i%10000 == 0 {
-			log.Infof("Writing %d rows to temp file...", i)
+			hexStr := fmt.Sprintf("%032x", i)
+			_, err = db.Exec("INSERT INTO downloaded_certs (md5,tbs_no_ct_md5) VALUES ($1,$2)", hexStr, hexStr)
+			if err, ok := err.(*pq.Error); ok {
+				log.Error("pq error:", err)
+			}
 		}
+	} else {
 
-		hexStr := fmt.Sprintf("%032x", i)
-		_, err = db.Exec("INSERT INTO downloaded_certs (md5,tbs_no_ct_md5) VALUES ($1,$2)", hexStr, hexStr)
-		if err, ok := err.(*pq.Error); ok {
-			log.Error("pq error:", err)
-		}
 	}
+
 
 	// Looks like it would take about 27 days to manually upload 2B records
 	// assuming no degradation in performance:
@@ -75,35 +94,35 @@ Options:
 	// TODO: try to do bulk upload without indexes using /data2/nsrg/ct/sha256_and_tbs_noct_fp.csv
 	// This should provide 1B+ records
 
-	//tempFname := "temp-copy.csv"
-	//file, err := os.Create(tempFname)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//writer := csv.NewWriter(file)
-	//writer.Write([]string{"md5", "tbs_no_ct_md5"})
-	//
-	//for i := 0; i < rows; i++ {
-	//	if i%10000 == 0 {
-	//		log.Infof("Writing %d rows to temp file...", i)
-	//	}
-	//
-	//	hexStr := fmt.Sprintf("%032x", i)
-	//	writer.Write([]string{hexStr, hexStr})
-	//}
-	//
-	//writer.Flush()
-	//file.Close()
-	//
-	//dir, err := os.Getwd()
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//fpath := filepath.Join(dir, tempFname)
-	//_, err = db.Exec("COPY downloaded_certs FROM '" + fpath + "' CSV HEADER")
-	//if err, ok := err.(*pq.Error); ok {
-	//	log.Error("pq error:", err)
-	//}
+	tempFname := "temp-copy.csv"
+	file, err := os.Create(tempFname)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	writer := csv.NewWriter(file)
+	writer.Write([]string{"md5", "tbs_no_ct_md5"})
+
+	for i := 0; i < rows; i++ {
+		if i%10000 == 0 {
+			log.Infof("Writing %d rows to temp file...", i)
+		}
+
+		hexStr := fmt.Sprintf("%032x", i)
+		writer.Write([]string{hexStr, hexStr})
+	}
+
+	writer.Flush()
+	file.Close()
+
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fpath := filepath.Join(dir, tempFname)
+	_, err = db.Exec("COPY downloaded_certs FROM '" + fpath + "' CSV HEADER")
+	if err, ok := err.(*pq.Error); ok {
+		log.Error("pq error:", err)
+	}
 
 }
