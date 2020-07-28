bab7f2852d0b9d38577ee68c857a4fc6d83507ab
zanema <amenaz1@gmail.com>
2020-07-28 10:32:51 -0500

    update chrome ct log extraction script

--- ctsync-pull/generateChromeLogsConfig.sh
+++ ctsync-pull/generateChromeLogsConfig.sh
@@ -1,1 +1,1 @@
-curl -s https://www.gstatic.com/ct/log_list/log_list.json | jq '.logs[]| "{\"name\":\"\(.description)\",\"url\":\"https://\(.url)\",\"batch_size\":10000}"' | tr '[:upper:]' '[:lower:]' | tr ' ' '_' | sed "s/'//g" | sed 's,\\,,g' | sed 's/^"//g' | sed 's/"$//g' | sed 's=/","batch_size=","batch_size=g'
+curl -s https://www.gstatic.com/ct/log_list/v2/log_list.json | jq '.operators[].logs[]| "{\"name\":\"\(.description)\",\"url\":\"https://\(.url)\",\"batch_size\":10000}"' | tr '[:upper:]' '[:lower:]' | tr ' ' '_' | sed "s/'//g" | sed 's,\\,,g' | sed 's/^"//g' | sed 's/"$//g'
