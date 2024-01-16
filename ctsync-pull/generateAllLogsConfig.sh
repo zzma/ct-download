6b9cb22e9e04453a9b24944fa26dd7290baff375
zzma <amenaz1@gmail.com>
2024-01-16 14:16:09 -0800

    update ct logs

--- ctsync-pull/generateAllLogsConfig.sh
+++ ctsync-pull/generateAllLogsConfig.sh
@@ -1,1 +1,1 @@
-:jqcurl -s https://www.gstatic.com/ct/log_list/v2/all_logs_list.json | jq '.operators[].logs[]| "{\"name\":\"\(.description)\",\"url\":\"\(.url)\",\"batch_size\":10000}"' | tr '[:upper:]' '[:lower:]' | tr ' ' '_' | sed "s/'//g" | sed 's,\\,,g' | sed 's/^"//g' | sed 's/"$//g'
+curl -s https://www.gstatic.com/ct/log_list/v2/all_logs_list.json | jq '.operators[].logs[]| "{\"name\":\"\(.description)\",\"url\":\"\(.url)\",\"batch_size\":10000}"' | tr '[:upper:]' '[:lower:]' | tr ' ' '_' | sed "s/'//g" | sed 's,\\,,g' | sed 's/^"//g' | sed 's/"$//g'
